0d22ff9f2e98004613b3adc81a15f014e45c781f
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-19 16:50:19 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,20 +2,29 @@ const start = document.querySelector('button[data-start]');
 const stop = document.querySelector('button[data-stop]');
 const body = document.querySelector("body");
 let timerId;
+stop.setAttribute("disabled" , "")
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
-  start.addEventListener("click" , onClick) ;
-  stop.addEventListener("click" , offClick);
+start.addEventListener("click", onClick);
+stop.addEventListener("click", offClick);
 
-  function onClick(evt) {
-    let randomColor = getRandomHexColor();
-    body.style.backgroundColor = randomColor;
-    const timerId =setInterval(onClick , 1000);
-   
-  }
+function updateColor() {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
 
-  function offClick (evt) {
-    clearInterval(timerId);
-  }
+function onClick() {
+  updateColor()
+  stop.removeAttribute("disabled" , "")
+  start.setAttribute("disabled" , "")
+  timerId = setInterval(updateColor, 1000);
+}
+
+
+function offClick() {
+  stop.setAttribute("disabled" , "")
+  start.removeAttribute("disabled")
+  clearInterval(timerId);
+}
