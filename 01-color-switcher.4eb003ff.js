790040586185409cf26e57e759275cfd37ab8b15
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-19 14:50:45 +0000

    Deploying to gh-pages from @ Oleg998/goit-js-hw-09@0d22ff9f2e98004613b3adc81a15f014e45c781f 🚀

--- 01-color-switcher.4eb003ff.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function t(e){let o=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;n.style.backgroundColor=o;setInterval(t,1e3)})),e.addEventListener("click",(function(t){clearInterval(undefined)}));
-//# sourceMappingURL=01-color-switcher.4eb003ff.js.map
