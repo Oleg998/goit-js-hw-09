4fe99a0b22fddc68e348ae9ef38e14bd4040de4b
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-19 13:54:05 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
+let timerId;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+  start.addEventListener("click" , onClick) ;
+  stop.addEventListener("click" , offClick);
+
+  function onClick(evt) {
+    let randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    const timerId =setInterval(onClick , 1000);
+   
+  }
+
+  function offClick (evt) {
+    clearInterval(timerId);
+  }
