900b8c1163feadcb09908f2cfe4335de11d4afe9
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-20 19:34:10 +0200

    main02

---
+++ src/js/02-timer.js
@@ -0,0 +1,66 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+const startBtn = document.querySelector('button[data-start]');
+startBtn.setAttribute('disabled', '');
+let userData;
+const options = {
+  altInput: true,
+  dateFormat: 'YYYY-MM-DD HH:MM',
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userData = selectedDates[0].getTime();
+    chekData(userData);
+  },
+};
+flatpickr('#datetime-picker', options);
+
+function chekData() {
+  const currentDate = new Date().getTime();
+  const setData = userData - currentDate;
+  if (setData < 0) {
+    alert('Please choose a date in the future');
+    return;
+  } else {
+    startBtn.removeAttribute('disabled');
+  }
+}
+
+function setTimer() {}
+
+startBtn.addEventListener('click', onClick);
+
+function onClick() {
+  timerId = setInterval(() => {
+    const deltaTime = userData - new Date().getTime();
+    if (deltaTime < 1000) {
+      clearInterval(timerId);
+    }
+    const result = convertMs(deltaTime);
+  }, 1000);
+}
+
+const setDays = document.querySelector('[data-days]');
+const setHours = document.querySelector('[data-hours]');
+const setMinutes = document.querySelector('[data-minutes]');
+const setSeconds = document.querySelector('[data-seconds]');
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  setDays.textContent = days;
+  setHours.textContent = hours;
+  setMinutes.textContent = minutes;
+  setSeconds.textContent = seconds;
+}
+
