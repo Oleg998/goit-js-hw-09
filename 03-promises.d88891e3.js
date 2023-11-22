120f700ea0222b56595a5039164a85d1cad1b27b
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-22 10:36:57 +0000

    Deploying to gh-pages from @ Oleg998/goit-js-hw-09@3c38de75ebe7534b7e71b5cc1373046550453103 🚀

---
+++ 03-promises.d88891e3.js
@@ -0,0 +1,2 @@
+parseFloat(document.querySelector('input[type="number"][name="delay"]')),parseFloat(document.querySelector('input[type="number"][name="step"]')),parseFloat(document.querySelector('input[type="number"][name="amount"]')),document.querySelector('button[type="submit"]');
+//# sourceMappingURL=03-promises.d88891e3.js.map
