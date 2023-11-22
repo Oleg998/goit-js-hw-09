3c38de75ebe7534b7e71b5cc1373046550453103
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-22 12:36:32 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+const delayInput=parseFloat( document.querySelector('input[type="number"][name="delay"]'));
+const stepInput =parseFloat (document.querySelector('input[type="number"][name="step"]'));
+const amountInput=parseFloat (document.querySelector('input[type="number"][name="amount"]'));
+const submitBtn=(document.querySelector('button[type="submit"]'));
+
+
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
